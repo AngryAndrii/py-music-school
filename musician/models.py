5a4a4a94489ab0d98123db198284cd9f9838a52b
Andrii Chychkan <mywork1728@gmail>
2025-12-30 13:01:41 +0200

    added models, migrations, model tests passed

--- musician/models.py
+++ musician/models.py
@@ -1,5 +1,21 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
+
+
+def age_validator(value):
+    if value < 14:
+        raise ValidationError(f"age must be over 13 years old")
 
 
 class Musician(models.Model):
-    pass
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+    instrument = models.CharField(max_length=63)
+    age = models.PositiveIntegerField(validators=[age_validator])
+    date_of_applying = models.DateTimeField(auto_now_add=True)
+
+    def is_adult(self):
+        return self.age >= 21
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
